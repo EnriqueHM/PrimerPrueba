1d66920103faf93a43ba7761e148678be3ccfc78
EnriqueHM <enriqhz@gmail.com>
2015-10-29 18:29:06 -0600

    Initial Commit

---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by Juan Enrique on 29/10/15.
+//  Copyright © 2015 Juan Enrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
